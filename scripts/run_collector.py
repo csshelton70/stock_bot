d44f8f04f1444006488162c28ef14014fb8fa51a
Scott Shelton <chris.scott.shelton@gmail.com>
2025-08-27 19:30:11 -0400

    adding some pylint disable strings

--- scripts/run_collector.py
+++ scripts/run_collector.py
@@ -4,15 +4,17 @@
 Refactored data collection runner script
 """
 
-# pylint:disable=broad-exception-caught,trailing-whitespace,line-too-long,logging-fstring-interpolation
+# pylint:disable=broad-exception-caught,trailing-whitespace,line-too-long,logging-fstring-interpolation, import-outside-toplevel
 
 import sys
+import os
 
-from apps.data_collector_app import DataCollectorApp 
-
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 def main() -> int:
     """Main entry point for data collection"""
+    from apps import DataCollectorApp 
+
     app = DataCollectorApp()
     f_exit_code = app.run()
     return f_exit_code
--- scripts/run_trading.py
+++ scripts/run_trading.py
@@ -3,14 +3,18 @@
 """
 Refactored trading system runner script
 """
+# pylint:disable=broad-exception-caught, trailing-whitespace, line-too-long,logging-fstring-interpolation, import-outside-toplevel
 
 import sys
+import os
 
-from apps.trading_system_app import TradingSystemApp
-
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 def main()->int:
     """Main entry point for trading system"""
+
+    from apps import TradingSystemApp
+
     app = TradingSystemApp()
     f_exit_code = app.run()
     return f_exit_code
